93fd319e95eab56f46a0c53af8256dab6395f886
Jorge Matos <jisapjisap@gmail.com>
2024-12-22 11:51:36 +0100

    Register

---
+++ src/components/TextField.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+
+
+
+const TextField = ({ 
+  classes = '',
+  helperText,
+  label,
+  name,
+  placeholder = '',
+  fieldClasses = '',
+  ...rest
+ }) => {
+  return (
+    <div className={`text-field-wrapper ${classes}`}>
+      <label 
+        htmlFor={name}
+        className='label-text'
+      >
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        className={`text-field ${fieldClasses}`}
+        {...rest} 
+      />
+
+      {helperText && (
+        <p className="helper-text">
+          {helperText}
+        </p>
+      )}
+    </div>
+  )
+}
+
+TextField.PropTypes = {
+  classes: PropTypes.string,
+  helperText: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  fieldClasses: PropTypes.string,
+}
+
+export default TextField
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import PageTitle from '../components/PageTitle'
+import { Link, Form } from 'react-router-dom'
+import { logoDark, logoLight } from '../assets/assets'
+import TextField from '../components/TextField'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <PageTitle title="Create an account" />
+      <div>
+        <div>
+          <Link>
+            <img src={logoLight} alt="logo light" width={133} height={24} />
+            <img src={logoDark} alt="logo dark" width={133} height={24}/>
+          </Link>
+
+          <div>
+            <h2>
+              Create an account
+            </h2>
+            <p>
+              Register today and gain access to powerful tools that will supercharge your ideas.
+            </p>
+            <Form
+              method="POST"
+              className=''
+            >
+              <TextField
+                type="text"
+                name="name"
+                label="Full Name"
+                placeholder="Full name"
+                required={true}
+                autoFocus={true}
+                classes=''
+              />
+              <TextField
+                type="email"
+                name="email"
+                label="Email"
+                placeholder="Email" 
+                required={true}
+              />
+              <TextField
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="Password"
+                required={true}
+              />
+            </Form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
