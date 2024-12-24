917fa7424d19eb97e5c941714a4df6ebb594d4f5
Jorge Matos <jisapjisap@gmail.com>
2024-12-24 18:11:36 +0100

    CircularProgress

---
+++ src/components/Progress.jsx
@@ -0,0 +1,23 @@
+
+
+import PropTypes from "prop-types";
+
+const CircularProgress = ({ 
+  classes = '',
+  size = '',
+ }) => {
+  return (
+    <div 
+      role="progressbar"
+      className={`circular-progress ${size} ${classes}`}
+    >  
+    </div>
+  )
+}
+
+CircularProgress.propTypes = {
+  classes: PropTypes.string,
+  size: PropTypes.string,
+}
+
+export default CircularProgress
