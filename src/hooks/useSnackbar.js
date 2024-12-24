1cd0f6885c61a62b6c87b14c7471a2a4d4109726
Jorge Matos <jisapjisap@gmail.com>
2024-12-24 19:30:28 +0100

    comments

--- src/hooks/useSnackbar.js
+++ src/hooks/useSnackbar.js
@@ -4,4 +4,4 @@
 import { useContext } from 'react'
 import { SnackbarContext } from '../context/SnackbarContext.jsx'
 
-export const useSnackbar = () => useContext(SnackbarContext)
+export const useSnackbar = () => useContext(SnackbarContext); // Función reutilizable que encapsula la lógica de acceso al contexto SnackbarContext.
