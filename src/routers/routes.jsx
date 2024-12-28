bb693715618ccc6346cb593b31baecac64c61bd8
Jorge Matos <jisapjisap@gmail.com>
2024-12-28 19:33:15 +0100

    login

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Register from "../pages/Register";
 import registerAction from "./actions/registerAction";
+import Login from "../pages/Login.jsx";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,11 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
     action: registerAction,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    
   },
 ]);
 
