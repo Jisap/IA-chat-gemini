d53cde1b71b1a120a8cb832060147cb3b1be07e6
Jorge Matos <jisapjisap@gmail.com>
2025-01-01 20:43:54 +0100

    Add MenuItem component and integrate it into Menu; update TopAppBar to include Menu and LinearProgress

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -24,17 +24,17 @@ CircularProgress.propTypes = {
 const LinearProgress = ({ classes = '' }) => {
 
   const progressbarVariant = {
-    start: { scaleY: 0 },
-    end: {
+    start: { scaleY: 0 },                // Inicializa la animación con la escala en el eje Y en 0
+    end: {                               // Termina la animación con la escala en el eje Y en 1
       scaleY: 1,
-      transition: {
+      transition: {                      // Configuración de la transición
         when: 'beforeChildren',
         duration: 0.2,
         ease: 'easeOut',
         delay: 0.5
       },
     },
-    exit: {
+    exit: {                              // Sale de la animación con la escala en el eje Y en 0
       scaleY: 0,
       transition: {
         duration: 0.1,
@@ -43,9 +43,9 @@ const LinearProgress = ({ classes = '' }) => {
     }
   }
 
-  const activeIndicatorVariant = {
-    start: { translateX: '-100%' },
-    end: { translateX: '100%' },
+  const activeIndicatorVariant = {       // Variantes para el indicador activo
+    start: { translateX: '-100%' },      // Inicializa la animación con la posición del indicador en -100% (esta oculto el contenido)
+    end: { translateX: '100%' },         // Termina la animación con la posición del indicador en 100% (está visible el contenido)
   }
 
 
@@ -56,10 +56,10 @@ const LinearProgress = ({ classes = '' }) => {
       initial='start'
       animate='end'
       exit='exit'
-      className={`linear-progress ${classes}`}
+      className={`linear-progress ${classes}`} // Agrega la clase "linear-progress" al elemento que el punto de transformación en la parte superior del elemento.
     >
       <motion.div 
-        variants={activeIndicatorVariant}
+        variants={activeIndicatorVariant}      // Provoca una transición de posición para el indicador activo desde -100 a 100.
         transition={{
           repeat: Infinity,
           duration: 1.5,
--- src/components/TopAppBar.jsx
+++ src/components/TopAppBar.jsx
@@ -3,23 +3,32 @@
 
 import React from 'react'
 import { IconBtn } from './Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigation } from 'react-router-dom'
 import { logoLight, logoDark } from '../assets/assets'
 import Avatar from './Avatar'
+import Menu from './Menu'
+import MenuItem from './MenuItem'
+import { LinearProgress } from './Progress'
+import { AnimatePresence } from 'framer-motion'
 
 
 const TopAppBar = () => {
+
+  const navigation = useNavigation();
+  const isNormalLoad = navigation.state === "loading" && !navigation.formData // Checkeamos que el estado sea loading y que no haya datos en el formulario
+
   return (
-    <header>
-      <div>
+    <header className='relative flex justify-between items-center h-16 px-4'>
+      <div className='flex items-center gap-1'>
         <IconBtn 
           icon="menu"
           title="Menu"
+          classes='lg:hidden'
         />
 
         <Link 
           to="/"
-          className=''
+          className='min-w-max max-w-max h-[24px] lg:hidden'
         >
           <img 
             src={logoLight}
@@ -43,7 +52,15 @@ const TopAppBar = () => {
         <IconBtn>
           <Avatar name="John Doe" />
         </IconBtn>
+
+        <Menu classes="">
+          <MenuItem labelText="Log out" />
+        </Menu>
       </div>
+
+      <AnimatePresence>
+        {isNormalLoad && <LinearProgress />}
+      </AnimatePresence>
     </header>
   )
 }
