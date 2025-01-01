567340c2ba9e47798daa3291306c2c2fb0132cf2
Jorge Matos <jisapjisap@gmail.com>
2025-01-01 12:22:52 +0100

    Add Menu component for flexible layout rendering

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Menu = ({ classes = "", children}) => {
+  return (
+    <div className={`menu ${classes}`}>
+      { children }
+    </div>
+  )
+}
+
+Menu.propTypes = {
+  classes: PropTypes.string,
+  children: PropTypes.any,
+}
+
+export default Menu
