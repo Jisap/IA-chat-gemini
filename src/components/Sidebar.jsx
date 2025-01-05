ee4808c3b0bb387a5bcc9ded2ec812f224a00d9a
Jorge Matos <jisapjisap@gmail.com>
2025-01-05 12:16:52 +0100

    Refactor Sidebar component; remove active state from sidebar and overlay, update nav link, and adjust IconBtn positioning

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { NavLink } from "react-router-dom"
 const Sidebar = () => {
   return (
     <>
-      <div className="sidebar active">
+      <div className="sidebar">
         <div className="sidebar-inner">
           <div className="h-16 grid items-center px-4 mb-4">
             <Logo />
@@ -28,7 +28,7 @@ const Sidebar = () => {
             <nav>
               <div className="relative group">
                 <NavLink 
-                  to="new-chat"
+                  to=""
                   className="nav-link"
                   title=""
                 >
@@ -37,18 +37,25 @@ const Sidebar = () => {
                   <div className="state-layer"></div>
                 </NavLink>
 
-                <IconBtn icon="delete" size="small" classes="" title="Delete" />
+                <IconBtn 
+                  icon="delete" 
+                  size="small" 
+                  classes="absolute top-1/2 right-1.5 -translate-y-[10px] z-10 opacity-0 group-hover:opacity-100 group:focus-within:opacity-100 hidden lg:grid" 
+                  title="Delete"   
+                />
               </div>
             </nav>
           </div>
 
-          <div>
+          <div 
+            className="mt-4 h-14 px-4 grid items-center text-labelLarge text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant border-t
+            border-light-surfaceContainerHigh dark:border-dark-surfaceContainerHigh truncate">
             &copy; 2024 All rights reserved
           </div>
         </div>
       </div>
 
-      <div className={`overlay active`}></div>
+      <div className={`overlay`}></div>
     </>
   )
 }
