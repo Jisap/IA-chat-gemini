3333316dc1a9dcb5a4eae50f897efbe947ae18bb
Jorge Matos <jisapjisap@gmail.com>
2025-01-08 16:23:28 +0100

    Enhance PromptField component with placeholder visibility toggle; implement input change handling and refactoring

--- src/components/promptField.jsx
+++ src/components/promptField.jsx
@@ -1,10 +1,18 @@
 import { motion } from "framer-motion"
 import { IconBtn } from "./Button"
-
+import { useRef, useCallback, useState } from "react"
 
 
 
 const PromptField = () => {
+
+  const inputField = useRef();
+  const inputFieldContainer = useRef();
+  const [placeholderShown, setPlaceholderShown] = useState(true);
+
+  const handleInputChange = useCallback(() => {
+    setPlaceholderShown(!inputField.current.innerText) // Si el inputField no tiene contenido, placeholderShown=true -> placeholder hidden
+  },[])
 
   const promptFieldVariant = {
     hidden: { scaleX: 0 },
@@ -31,15 +39,18 @@ const PromptField = () => {
       variants={promptFieldVariant}
       initial="hidden"
       animate="visible"
+      ref={inputFieldContainer}
     >
       <motion.div
-        className="prompt-field"
+        className={`prompt-field ${placeholderShown ? "" : "after:hidden"}`}
         contentEditable={true}
         role="textbox"
         aria-multiline={true}
         aria-label="Enter a prompt here"
         data-placeholder="Enter a prompt here"
         variants={promptFieldChildrenVariant}
+        ref={inputField}
+        onInput={handleInputChange}
       />
       <IconBtn 
         icon="send"
