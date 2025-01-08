0facb4c7d81140e5a4184ad0ae4e1f074134f466
Jorge Matos <jisapjisap@gmail.com>
2025-01-08 17:19:09 +0100

    Enhance PromptField component with multiline support; implement input handling and cursor management

--- src/components/promptField.jsx
+++ src/components/promptField.jsx
@@ -9,10 +9,39 @@ const PromptField = () => {
   const inputField = useRef();
   const inputFieldContainer = useRef();
   const [placeholderShown, setPlaceholderShown] = useState(true);
+  const [isMultiline, setIsMultiline] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = useCallback(() => {
-    setPlaceholderShown(!inputField.current.innerText) // Si el inputField no tiene contenido, placeholderShown=true -> placeholder hidden
-  },[])
+    if(inputField.current.innerText === "\n"){                     // Si se pulsa enter
+      inputField.current.innerHTML = "";                           // la nueva linea no tiene contenido
+    }
+    
+    setPlaceholderShown(!inputField.current.innerText)             // Si el inputField no tiene contenido, placeholderShown=true -> placeholder hidden
+    setIsMultiline(inputFieldContainer.current.clientHeight > 64)  // Si el inputFieldContainer tiene más de 64 px de alto, es multilinea -> multiline=true -> rounded-large
+    setInputValue(inputField.current.innerText.trim())
+  },[]);
+
+  const moveCursorToEnd = useCallback(() => {
+    const editableElem = inputField.current;                       // Referencia al elemento editable
+    const range = document.createRange();                          // Representa un fragmento del documento que puede contener nodos
+    const selection = window.getSelection();                       // Representa el rango de texto seleccionado por el usuario o la posición actual del cursor
+
+    range.selectNodeContents(editableElem);                        // Establece el Range para abarcar todo el contenido del editableElem.
+    range.collapse(false);                                         // Colapsa el Range a su punto final, moviendo efectivamente el cursor al final del contenido.
+    selection.removeAllRanges();                                   // Elimina cualquier selección existente.       
+    selection.addRange(range);                                     // Agrega el Range recién creado a la selección, colocando el cursor al final del contenido.
+  }, [])
+
+  const handlePaste = useCallback((e) => {
+    e.preventDefault();
+    inputField.current.innerText += e.clipboardData.getData('text')
+    
+    handleInputChange()
+    moveCursorToEnd()
+  },[handleInputChange, moveCursorToEnd]);
+
+  
 
   const promptFieldVariant = {
     hidden: { scaleX: 0 },
@@ -35,7 +64,7 @@ const PromptField = () => {
 
   return (
     <motion.div
-      className="prompt-field-container"
+      className={`prompt-field-container ${isMultiline ? "rounded-large" : ""}`}
       variants={promptFieldVariant}
       initial="hidden"
       animate="visible"
@@ -51,12 +80,13 @@ const PromptField = () => {
         variants={promptFieldChildrenVariant}
         ref={inputField}
         onInput={handleInputChange}
+        onPaste={handlePaste}
       />
       <IconBtn 
         icon="send"
         title="Submit"
         size="large"
-        className="ms-auto"
+        className="ms-auto mb-1.5"
         variants={promptFieldChildrenVariant}
       />
       <div className="state-layer"></div>
